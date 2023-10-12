90223b9fac536131cdd996339650f49d82c4873c
Luca Morselli <luca.morselli8@gmail.com>
2023-10-12 22:47:59 +0200

    added a simple argument parser

--- main.cpp
+++ main.cpp
@@ -6,21 +6,75 @@
 #include "renderer.hh"
 #include "spin_lattice.hh"
 
-int main() {
+int main(int argc, char* argv[]) {
+
+    // Parsing arguments
+    int N = 0; // number of images to be generated
+    int L = 0; // lattice size (LxL)
+
+     for (int i = 1; i < argc; i++) 
+     {
+
+        std::string arg = argv[i];
+
+        if (arg == "--num-generated-images" || arg == "-N") 
+        {
+            
+            if (i + 1 < argc) 
+            {
+                N = std::stoi(argv[i + 1]);
+                i++;  // Skip the next argument
+            } 
+            else 
+            {
+                std::cerr << "Error: --num-generated-images option requires an argument." << std::endl;
+                return 1;
+            }
+        }
+        else if (arg == "--lattice-size" || arg == "-L") 
+        {
+            
+            if (i + 1 < argc) 
+            {
+                L = std::stoi(argv[i + 1]);
+                i++;  // Skip the next argument
+            } 
+            else 
+            {
+                std::cerr << "Error: --lattice-size option requires an argument." << std::endl;
+                return 1;
+            }
+        }
+        else 
+        {
+            std::cerr << "Error: Unknown argument '" << arg << "'" << std::endl;
+            return 1;
+        }
+    }
+
+    if(N==0) 
+    {
+        std::cerr << "Error: option --num-generated-images is required" << std::endl;
+        exit(1);
+    }
+    if(L==0)
+    {
+        std::cerr << "Error: option --lattice-size is required" << std::endl;
+        exit(1);
+    } 
 
     // Print program name and version
     splash_screen();
 
-    int N = 500;
 
-    for(int l = 1; l <= 100; l++)
+    for(int n = 1; n <= N; n++)
     {
 
         // Declare a spin lattice object 
-        SpinLattice lattice = SpinLattice(N);
+        SpinLattice lattice = SpinLattice(L);
 
         // Annealing procedure
-        std::cout << std::endl << "Starting annealing procedure for map #" << l << std::endl;
+        std::cout << std::endl << "Starting annealing procedure for map #" << n << std::endl;
         lattice.anneal(190000000);
         
         // Remove noisy pixel by "freezing" the lattice
@@ -35,7 +89,7 @@ int main() {
         // generating amenities on map using the heightmap
         // Generate a set of rivers
         std::cout << std::endl << "Generating rivers . . ." << std::endl;
-        for (size_t i = 0; i < 30; i++)
+        for (size_t i = 0; i < 20; i++)
         {
             map.generate_river();
         }
@@ -57,7 +111,7 @@ int main() {
         std::cout << "Rendering the map . . ." << std::endl;
         Renderer renderer = Renderer();
 
-        std::string filename = "maps/map"+std::to_string(l)+".bmp";
+        std::string filename = "map"+std::to_string(n)+".bmp";
         renderer.render(&map, filename);
 
         char buffer[200];
